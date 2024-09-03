c348f2ef7877642b4eab211ac0cc2ca424db3931
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-09-03 20:45:47 +0200

    Login route, controller and view

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 //It parses the requests
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
