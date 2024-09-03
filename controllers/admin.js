fbd5ba5a70aed297563c54bd6f2a5c90e5a880b1
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-09-03 09:28:00 +0200

    Delete a product with mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,7 +70,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log('Product deleted');
       res.redirect('/admin/products');
