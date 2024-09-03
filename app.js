f151f767dab41a26e5f5bbf753d5cdffe72cc873
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-09-03 10:06:21 +0200

    User passed in every request

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user')
+const User = require('./models/user')
 
 const app = express();
 
@@ -23,16 +23,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to pass a user to every incoming request
-// app.use((req, res, next) => {
-//   User.findById('66cf3efa91a6aca071476df5')
-//     .then(user => {
-//       //We assing a new property to the req object
-//       req.user = new User(user.username, user.email, user.cart, user._id);
-//       // Goes to the next request
-//       next();
-//     })
-//     .catch(err => console.error(err));
-// });
+app.use((req, res, next) => {
+  User.findById('66d6c23edbc05c117ae178d0')
+    .then(user => {
+      //We assing a new property to the req object
+      req.user = user;
+      // Goes to the next request
+      next();
+    })
+    .catch(err => console.error(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -42,6 +42,19 @@ app.use(errorController.get404);
 mongoose
   .connect('mongodb+srv://srgibosque:NNQ3XHX3%40!8Nyrn@cluster0.oyxb5.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
   .then(result => {
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'Sergi',
+            email: 'sbosque@test.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      });
     app.listen(3000);
   })
   .catch(err => console.error(err));
