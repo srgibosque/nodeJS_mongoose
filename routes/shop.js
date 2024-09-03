c41136a0dbd500ef1f2993436c63661b12d6c154
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-09-03 17:50:39 +0200

    Delete items from Cart

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
-// router.post('/cart-delete-item', shopController.postCartDeleteItem);
+router.post('/cart-delete-item', shopController.postCartDeleteItem);
 // router.get('/orders', shopController.getOrders);
 // router.post('/create-order', shopController.postOrders);
 // router.get('/checkout', shopController.getCheckout);
