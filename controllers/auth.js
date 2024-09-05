a55ca77b85f7ab9f428b3e2e4d7596cfe2300c70
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-09-05 10:19:21 +0200

    Setting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.get('Cookie'));
+  const isLoggedIn = req.get('Cookie').split('=')[1].trim();
+  console.log(isLoggedIn);
   res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login'
   });
+}
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
 }
